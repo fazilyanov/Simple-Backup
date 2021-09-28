329945b8f21943d46f2030ae83d8b50a0a8fb48b
a.fazl <a.fazilyanov@gmail.com>
2021-09-28 17:44:03 +0500

    reg

--- src/Program.cs
+++ src/Program.cs
@@ -1,44 +1,75 @@
 ﻿using System;
 using System.Diagnostics;
+using Microsoft.Win32;
 
 
 namespace FastBackup
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            SetRegistry();
+
+            //using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"HKEY_CLASSES_ROOT\*\shell"))
+            //{
+            //    key.SetValue("Title", $".NET Core Rocks! {DateTime.UtcNow:MM/dd/yyyy-HHmmss}");
+            //}
+
+
+            //         if (args.Length == 0)
+            //         {
+            //             Console.WriteLine("Не указан путь источника");
+            //             Console.ReadLine();
+            //         }
+
+            //         string sourceName = @"d:\33";
+            //         string targetName = @"d:\111.7z";
+
+            //         var p = new ProcessStartInfo
+            //         {
+            //             FileName = "7z.exe",
+            //             Arguments = "a -t7z \"" + targetName + "\" \"" + sourceName + "\" -mx=9"
+            //         };
+
+
+            //         Process x = Process.Start(p);
+            //         x.WaitForExit();
+
+            //Console.WriteLine(args[0]);
+            Console.ReadLine();
+        }
+
+        private static void SetRegistry()
+        {
+            const string keyName = @"*\shell\FastBackup\";
+            using (var key = Registry.ClassesRoot.CreateSubKey(keyName))
             {
-                Console.WriteLine("Не указан путь источника");
-                Console.ReadLine();
+                if (key != null)
+                {
+                    key.SetValue(null, "FastBackup");
+                    using (var skey = Registry.ClassesRoot.CreateSubKey(keyName+"command"))
+                    {
+                        if (skey!=null)
+                        {
+                            skey.SetValue(null, @"'C:\Users\a.fazilyanov\AppData\Local\Programs\Microsoft VS Code\Code.exe' '%1'");
+                        }
+                        
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("fuuuu");
+                }
             }
 
-            string sourceName = @"d:\33";
-            string targetName = @"d:\111.7z";
-
-            // 1
-            // Initialize process information.
-            //
-            ProcessStartInfo p = new ProcessStartInfo();
-            p.FileName = "7z.exe";
-
-            // 2
-            // Use 7-zip
-            // specify a=archive and -tgzip=gzip
-            // and then target file in quotes followed by source file in quotes
-            //
-            p.Arguments = "a -t7z \"" + targetName + "\" \"" + sourceName + "\" -mx=9";
-            //p.WindowStyle = ProcessWindowStyle.Hidden;
-
-            // 3.
-            // Start process and wait for it to exit
-            //
-            Process x = Process.Start(p);
-            x.WaitForExit();
-
-			Console.WriteLine(args[0]);
-            Console.ReadLine();
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\edi_wang"))
+            {
+                key.SetValue("Title", $".NET Core Rocks! {DateTime.UtcNow:MM/dd/yyyy-HHmmss}");
+            }
         }
     }
 }
